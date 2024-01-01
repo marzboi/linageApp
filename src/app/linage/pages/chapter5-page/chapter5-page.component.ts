3c5e428b4f9138ea650b46a368f9e47519d99238
Marco Fratini <marcofratini@outlook.com>
2024-01-01 14:05:38 -0500

    Add chapter 5 and resources

--- src/app/linage/pages/chapter5-page/chapter5-page.component.ts
+++ src/app/linage/pages/chapter5-page/chapter5-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-chapter5-page',
+  selector: 'linage-chapter5-page',
   templateUrl: './chapter5-page.component.html',
-  styleUrls: ['./chapter5-page.component.scss']
+  styleUrls: ['./chapter5-page.component.scss'],
 })
 export class Chapter5PageComponent {
+  @Output() onTrackChange: EventEmitter<number> = new EventEmitter<number>();
+  private pageIndex: number = 5;
 
+  handleEmit() {
+    this.onTrackChange.emit(this.pageIndex);
+  }
 }
