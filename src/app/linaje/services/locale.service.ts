b619a352db63847fcb5f07964d8a0cb883621555
Marco Fratini <122683263+marzboi@users.noreply.github.com>
2024-10-08 09:13:43 +0200

    Add baseline for locale service

--- src/app/linaje/services/locale.service.ts
+++ src/app/linaje/services/locale.service.ts
@@ -1,8 +1,48 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable, signal, computed } from '@angular/core';
+
+interface State {
+  currentLocale: 'es' | 'en';
+  localeContent: {
+    introPage: {
+      title: string;
+      subtitle: string;
+    };
+  };
+}
 
 @Injectable({
   providedIn: 'root',
 })
 export class LocaleService {
-  constructor() {}
+  private initialLocale: 'es' = 'es';
+
+  private spanishLocale = {
+    introPage: {
+      title: 'Linaje',
+      subtitle: 'Una exposición digital visual y sonora',
+    },
+  };
+
+  private englishLocale = {
+    introPage: {
+      title: 'Lineage',
+      subtitle: 'A digital visual and sound exhibition',
+    },
+  };
+
+  #state = signal<State>({
+    currentLocale: this.initialLocale,
+    localeContent: this.spanishLocale,
+  });
+
+  public currentLocale = computed(() => this.#state().currentLocale);
+  public localeContent = computed(() => this.#state().localeContent);
+
+  public changeLocale() {
+    this.#state.set({
+      currentLocale: this.currentLocale() === 'es' ? 'en' : 'es',
+      localeContent:
+        this.currentLocale() === 'es' ? this.englishLocale : this.spanishLocale,
+    });
+  }
 }
